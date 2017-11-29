b35eca610aacf613eb54a5000b3f91e6a77de010
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2017-11-29 17:12:57 +1300

    WIP to getNumPages

--- index.js
+++ index.js
@@ -24,6 +24,33 @@ async function run() {
 
   await page.waitForNavigation();
 
+  const userToSearch = 'john';
+  const searchUrl = `https://github.com/search?q=${userToSearch}&type=Users&utf8=%E2%9C%93`
+  
+  await page.goto(searchUrl);
+  await page.waitFor(2*1000);
+
+  const LENGTH_SELECTOR_CLASS = 'user-list-item';
+
+  let listLength = await page.evaluate((sel) => {
+    return document.getElementsByClassName(sel).length;
+  }, LENGTH_SELECTOR_CLASS);
+
+  const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > a > em'
+  const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a'
+
+  for (let i = 1; i <= listLength; i ++) {
+    // change the index to the next child
+    let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX", i);
+    let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX", i);
+
+    let username = await page.evaluate((sel) => {
+      return document.querySelector(sel).getAttribute('href').replace('/', '');
+    }, usernameSelector);
+    
+  }
+
+
   setTimeout(() => { browser.close(); }, 3000);  
    
 }
